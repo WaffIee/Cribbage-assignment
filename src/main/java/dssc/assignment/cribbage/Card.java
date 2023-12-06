4978b2eef0959d48946dbb8cee7687796fda37e3
Wafaa Bajjouk <wafaabajjouk@Wafaas-MacBook-Pro.local>
2023-12-06 11:33:35 +0100

    Card and card parser

--- src/main/java/dssc/assignment/cribbage/Card.java
+++ src/main/java/dssc/assignment/cribbage/Card.java
@@ -2,11 +2,44 @@ package dssc.assignment.cribbage;
 
 public class Card {
 
-    public String rank() {
-        return "5";
+    private String rank;
+    private Suite suite;
+
+    public Card(String rank, Suite suite) {
+        this.rank = rank;
+        this.suite = suite;
     }
 
-    public Suite suite() {
-        return Suite.HEART;
+    public int getValue() {
+        // Implement your logic to get the value of the card
+        // In Cribbage, Ace counts as 1, and face cards (Jack, Queen, King) count as 10
+        // Number cards have their face value
+        if ("A".equals(rank)) {
+            return 1;
+        } else if ("J".equals(rank) || "Q".equals(rank) || "K".equals(rank)) {
+            return 10;
+        } else {
+            return Integer.parseInt(rank);
+        }
+    }
+
+    public int getRankValue() {
+        // Implement your logic to get the numeric value of the rank
+        // In Cribbage, Ace is low, so it has the lowest rank value
+        if ("A".equals(rank)) {
+            return 1;
+        } else if ("J".equals(rank)) {
+            return 11;
+        } else if ("Q".equals(rank)) {
+            return 12;
+        } else if ("K".equals(rank)) {
+            return 13;
+        } else {
+            return Integer.parseInt(rank);
+        }
+    }
+
+    public Suite getSuite() {
+        return suite;
     }
 }
--- src/main/java/dssc/assignment/cribbage/CardParser.java
+++ src/main/java/dssc/assignment/cribbage/CardParser.java
@@ -2,6 +2,31 @@ package dssc.assignment.cribbage;
 
 public class CardParser {
     public static Card parseCard(String cardAsText) {
-        return new Card();
+        if (cardAsText.length() != 2) {
+            throw new IllegalArgumentException("Invalid card format: " + cardAsText);
+        }
+
+        String rank = cardAsText.substring(0, 1);
+        String suiteStr = cardAsText.substring(1, 2);
+
+        Suite suite;
+        switch (suiteStr) {
+            case "H":
+                suite = Suite.HEART;
+                break;
+            case "C":
+                suite = Suite.CLUB;
+                break;
+            case "D":
+                suite = Suite.DIAMOND;
+                break;
+            case "S":
+                suite = Suite.SPADE;
+                break;
+            default:
+                throw new IllegalArgumentException("Invalid suite: " + suiteStr);
+        }
+
+        return new Card(rank, suite);
     }
 }
